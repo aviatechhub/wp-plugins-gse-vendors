824773f5adf845cfaafc5df351a4ce87e622bb7a
Archie To <toanhminh0412@gmail.com>
2025-08-26 06:02:01 -0700

    Add plugin header

---
+++ gse-vendors.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: GSE Vendors
+Description: Admin-only UI and REST endpoints for managing vendor basic information.
+Version: 0.1.0
+Author: GSE
+License: GPLv2 or later
+Text Domain: gse-vendors
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
